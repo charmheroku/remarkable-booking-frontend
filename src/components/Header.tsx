b1387a5af4510c30a0850f5a1c7373aaf25b609d
Oleksii Ivanov <pcharm563@gmail.com>
2023-02-03 09:17:14 +0100

    add Dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
-import { FaMoon } from "react-icons/fa";
-import { RiHotelFill} from "react-icons/ri";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { RiHotelFill } from "react-icons/ri";
 import {
   Box,
   Button,
   HStack,
   IconButton,
   useDisclosure,
+  LightMode,
+  useColorMode,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import LoginModal from "./LoginModal";
@@ -22,6 +25,9 @@ export default function Header() {
     onClose: onSignUpClose,
     onOpen: onSignUpOpen,
   } = useDisclosure();
+  const { toggleColorMode } = useColorMode();
+  const logoColor = useColorModeValue("red.500", "red.500");
+  const Icon = useColorModeValue(FaMoon, FaSun);
   return (
     <HStack
       justifyContent={"space-between"}
@@ -29,21 +35,24 @@ export default function Header() {
       px={10}
       borderBottomWidth={1}
     >
-      <Box color="red.500">
+      <Box color={logoColor}>
         <Link to={"/"}>
           <RiHotelFill size={"48"} />
         </Link>
       </Box>
       <HStack spacing={2}>
         <IconButton
+          onClick={toggleColorMode}
           variant={"ghost"}
           aria-label="Toggle dark mode"
-          icon={<FaMoon />}
+          icon={<Icon />}
         />
         <Button onClick={onLoginOpen}>Log in</Button>
-        <Button onClick={onSignUpOpen} colorScheme={"red"}>
-          Sign up
-        </Button>
+        <LightMode>
+          <Button onClick={onSignUpOpen} colorScheme={"red"}>
+            Sign up
+          </Button>
+        </LightMode>
       </HStack>
       <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
       <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
